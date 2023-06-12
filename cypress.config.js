d5de2747eaee5b145743254ce99a1821e0032ac7
“GULNURKIRCAER” <gulnur1984@yahoo.com>
2023-06-12 20:46:18 +0100

    new tests

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl:'https://practice.cybertekschool.com/',
+    video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -16,27 +16,73 @@ describe('Find or get elements by using different locators', () => {
 
         })
         
-        cy.get("input[name='username']").type('tomsmith');
+        // cy.get("input[name='username']").type('tomsmith');
 
-        cy.wait(3000);
+        // cy.wait(3000);
         
-        cy.get("input[name='password']").type('SuperSecretPassword');
+        // cy.get("input[name='password']").type('SuperSecretPassword');
 
-        cy.wait(3000);
+        // cy.wait(3000);
 
-        cy.get("button[type='submit']").click();
+        // cy.get("button[type='submit']").click();
 
-        cy.wait(3000);
+        // cy.wait(3000);
 
-        cy.get('#flash').should("be.visible");
+        // cy.get('#flash').should("be.visible");
 
-        // cy.get('#flash').contains('You logged into a secure area!');
+        // // cy.get('#flash').contains('You logged into a secure area!');
         
-        //assertion
-        cy.get('#flash').should('include.text','You logged into a secure area!');
+        // //assertion
+        // cy.get('#flash').should('include.text','You logged into a secure area!');
       })
       
-    it('Check different locators strategies',()=>{
+    it.skip('Check different locators strategies',()=>{
+      //by css locator
+      cy.get("input[name='username']").type("CydeoStudent");
+
+      //by attribute name and value
+      cy.get("[type='text']").clear(); //clear what is typed
+
+      //tagName
+      cy.get("input").each((item,index,list)=>{
+        
+        //assert the lenght of the list is 2
+        // ayni degerde birden fazla element oldugunde kullanilir
+
+        expect(list).to.have.length(2);
+        expect(item).to.have.attr("type");
+      })
+      //by attribute name
+      cy.get('[type]');
+
+      //cy className
+      cy.get('.btn.btn-primary');
       
-    })    
+      //by id
+      cy.get('#wooden_spoon');
+
+      cy.get('button').should('contain','Login').click();
+    })  
+    
+    it('Check finding elements by taveling through DOM',()=>{
+      
+      //travel to find the login button
+      cy.get("input[name='username']").parents('form').find('button').should('contain','Login').click();
+    })
+
+    it.only('Check Different type of Assertions',()=>{
+      
+      //should assertions
+      cy.get("#wooden_spoon")
+      .should('contain','Login')
+      .and('have.class','btn btn-primary');
+
+      //expect assertions
+      cy.get("#wooden_spoon").then((buttonElement)=>{
+        expect(buttonElement).to.have.text('Login');
+        expect(buttonElement).to.have.class('btn btn-primary');
+
+      })
+      
+    })
 })
