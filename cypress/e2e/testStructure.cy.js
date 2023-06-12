bdeebf27971536d2e3198c3d3c57d1d1a38c6dea
“GULNURKIRCAER” <gulnur1984@yahoo.com>
2023-06-12 13:42:08 +0100

    new

--- cypress/e2e/testStructure.cy.js
+++ cypress/e2e/testStructure.cy.js
@@ -23,8 +23,29 @@ afterEach(()=>{
 
 it('Open a web application',()=>{
 
-    cy.get('.list-group > :nth-child(1) > a').click();
+    cy.visit('/hovers');
 
     cy.get('.nav-link').click();
 })
+
+it('Test 2',()=>{
+  
+  expect(false).to.equal(false);
+})
+
+it('Test 3',()=>{
+
+  expect(false).not.to.equal(true);
+})
+
+it('Test 4',()=>{
+
+  expect(5).to.equal(5);
+})
+
+it('Test 5',()=>{
+
+  expect(true).to.equal('5'==5);
+})
+
 })
